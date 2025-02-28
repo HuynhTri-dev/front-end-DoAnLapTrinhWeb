ba507ea9b84db0458403527300c0b2a81f5b8a36
TriHuynh <huynhminhtri1109@gmail.com>
2025-02-28 19:48:13 +0700

    Update assets and enhance styling for login and home pages; improve animations and add startup section functionality

--- utils/AnimationHome.js
+++ utils/AnimationHome.js
@@ -1,16 +1,31 @@
 $(window).on("scroll", function () {
   let scrollPosition = $(window).scrollTop();
-  let opacity = Math.min(1, scrollPosition / 500);
+  let opacity = Math.max(0, 1 - scrollPosition / 500);
+
   $("#rocket").css({
-    transform: `translate(${scrollPosition / 5}px, -${
-      scrollPosition / 5
-    }px) rotate(45deg)`,
+    transform: `translate(${scrollPosition / 5}px, ${-scrollPosition / 5}px)`,
   });
 
   $("#fire").css({
     opacity: opacity,
-    transform: `translate(${scrollPosition / 5}px, -${
-      scrollPosition / 5
-    }px) rotate(225deg)`,
+    transform: `translate(${scrollPosition / 5}px, ${-scrollPosition / 5}px)`,
   });
+});
+
+let $list = $(".startup-list");
+let itemWidth = $(".startup-item").outerWidth(true);
+let position = 0;
+
+$("#nextBtn").click(function () {
+  if (position > -itemWidth * ($(".startup-item").length - 3)) {
+    position -= itemWidth;
+    $list.css("transform", `translateX(${position}px)`);
+  }
+});
+
+$("#prevBtn").click(function () {
+  if (position < 0) {
+    position += itemWidth;
+    $list.css("transform", `translateX(${position}px)`);
+  }
 });
